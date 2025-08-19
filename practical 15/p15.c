f778ddcc664e886bc6193d594c67ae811e7fc05f
Darsheel111 <shiroyadarsheel@gmail.com>
2025-08-19 09:51:36 +0530

    Add files via upload

---
+++ practical 15/p15.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+int second;
+printf("enter the number for you want set countdown");
+scanf("%d",&second);
+
+for(second;second;second--){
+printf("%d\n",second);
+sleep(1);}
+}
