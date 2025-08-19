32564511aaed0b513a7c996e4c906cb7c2427bda
Darsheel111 <shiroyadarsheel@gmail.com>
2025-08-19 11:06:00 +0530

    Add files via upload

---
+++ practical 17/pr17.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+float distance;
+int minute=0;
+while(distance>=0 || minute>0){
+distance+=0.5;
+minute++;
+printf("Minute %d : distance covered = %.1f \n", minute,distance);
+if (distance>=10){
+printf("marathon complete");
+break;
+}
+}
+return 0;
+}
