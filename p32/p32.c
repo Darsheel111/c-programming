130c4f4f3830c29570b9bc37788180995d5ad77c
Darsheel111 <shiroyadarsheel@gmail.com>
2025-11-18 10:00:15 +0530

    Add files via upload

---
+++ p32/p32.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+void reverse(char *,int);
+int main(){
+    printf("Name:-Darsheel Shiroya\n");
+    printf("id:- 25ce116\n");
+    FILE *fp;
+    int j =0;
+    fp = fopen("Demo.txt","r");
+    if (fp == NULL)
+    {
+        perror("Error Reading File");
+    }
+    
+    fseek(fp,0,SEEK_END);
+    long size = ftell(fp);
+    fseek(fp,0,0);
+
+    char *data= (char *)malloc(size);
+    fread(data,size,1,fp);
+    
+    
+    
+    reverse(data,size);
+
+    free(data);
+    fclose(fp);
+}
+
+void reverse(char *data,int size){
+    int i=0,j=0,k=0;
+    for (int l = 0; l < size+1; l++)
+    {
+        if (data[l] == ' ' || data[l] == '\0')
+        {
+            for (int m = l; m >= j; m--)
+            {
+                printf("%c",data[m]);
+            }
+            j=l;
+        }
+        
+    }
+    
+}   
